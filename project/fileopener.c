438784a8a87683fb443e2ff2399695fb6b17fcb6
Clonethis <jirka.brtnik@post.cz>
2022-01-14 23:01:26 +0100

    some update

--- project/fileopener.c
+++ project/fileopener.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-#include <string.h>
 
 
 
@@ -41,31 +40,21 @@ int eloCALC (int* usersELO,int whichToCalc,int redWin){
 
 struct Player{
   int id;
-  char name[20];
-  int elo;
-  char teamPlays;
-  int gamesPlayed;
+  char* name;
+  int* elo;
+  char* teamPlays;
+  int* gamesPlayed;
 
 
 };
-typedef struct game{
+struct Game{
   unsigned int id;
-  char blu[20];
-  char red[20];
-  // char starts
-  char playersStatsBlu[40];
-  char playersStatsRed[40];
-}Game;
-
-int numberOfRows(FILE *file){
-int count = 0;
-  for(char c=getc(file);c!=EOF;c=getc(file)){
-  if( c == '\n'){
-    count++;
-  }}
-  rewind(file);
-  return count;
-}
+  unsigned int blu[3];
+  unsigned int red[3];
+  // players stats
+  unsigned int playersStatsBlu[9];
+  unsigned int playersStatsRed[9];
+};
 int numberOfGames(FILE *file){
   rewind(file);
   int count =0;
@@ -75,7 +64,7 @@ int numberOfGames(FILE *file){
             { 
               // c == m
               if(c==109){
-                printf("New match");
+                printf("Iom running");
                 count++;
               }
               c=getc(file);
@@ -84,94 +73,9 @@ int numberOfGames(FILE *file){
   return count;
 
 }
-int calcLineLength(FILE *file){
-  char c;
-  int count = 0;
-  while(c!='\n'){
-    c=getc(file);
-    count++;
-  }
-  // returns at the start of line
-  fseek(file,sizeof(char),count);
-  return count;
-}
-Game* loadGames(FILE *file,int length){
+struct playerCreator( Player players,id,name,){
 
-// loading lines instead of chars
-Game games[length];
-  char buffer[200];
-  int row=0,col = 0,id=0;
-  Game *ptr_G = &games[id];
-
-  while (fgets(buffer, 200, file)) {
-    ptr_G = &games[id];  
-    col = 0;
-    row++;
-    if(row%6==0){
-      continue;
-      id++;
-    }
-     char* value = strtok(buffer, ",");
-      while (value) {
-        if((row-1)%6 ==0){
-// load red id's
-printf("Irun");
-         strcpy(ptr_G->red,*value);
-        }
-        if((row-2)%6 ==0){
-// stats for red
-        }
-        if((row-3)%6 == 0){
-// load blu id's
-// when loaded check if blue same as red
-        }
-        if((row-4)%6 == 0){
-// stats for blu
-
-        }
-        if((row-5)%6 == 0){
-// who won
-        }
-        value = strtok(NULL,",");
-        row ++;
-
-                
-            }
-  
-            printf("\n");
-            
-return &games;
-}}
-// struct playerCreator( Player players,id,name,){
-
-//  }
-void populatePlayersId(int* playerIds,int len,FILE *file){
-  rewind(file);
-  printf("players ids %d",*(playerIds));
-  char c;
-  char id[10];
-  int count = 0, playerNum = 0;
-  while(c!=EOF){
-    // 48->56 nums; 59 = ";" 44=","
-    c=getc(file);
-    printf("%c",c);
-    if(c=='\n'){
-      c = getc(file);
-      while(c!=44){
-        id[count] = c;
-        count++;
-        // printf("ints: %c",c);
-        c = getc(file);
-      }
-      // printf("id %d\n",atoi(id));
-      playerIds[playerNum] = atoi(id);
-      id[0]=0;
-      count = 0;
-      playerNum++;
-    }
-  }
-  rewind(file);
-}
+ }
 
 int main(int agc,char **argv)
 {  
@@ -179,94 +83,79 @@ int main(int agc,char **argv)
   FILE *fplayer,*fgames;
   char c,before = -2;
   char *id;
-  // id = malloc(siqzeof(char)*8);
-  // struct Player player;
+  id = malloc(sizeof(char)*8);
+  Player player;
   // int* players;
-  // int user =-1,charCount=0;
-  // size_t count=0,nmOfLines=0;
+  int user =-1,charCount=0;
+  size_t count=0,nmOfLines=0;
   fplayer = fopen(argv[1], "r");
   fgames = fopen(argv[2], "r");
-  if(fplayer==NULL || fgames==NULL){
-    printf("couldn't open file");
-    return 1;
-  }
-  int num = numberOfRows(fgames);
-  int players = numberOfRows(fplayer);
-  
-  int* playerIds = (int *)malloc(players*sizeof(int));
-  printf("players ids main %d\n",playerIds);
-  printf("players & ids main %d\n",&playerIds);
-  printf("players * ids main %d\n",*playerIds);
-  populatePlayersId(&playerIds,players,fplayer);
-  // for(int i = 0; i<= players;i++){
-  //   // printf("player ids: %d",playerIds[i]);
-  // }
-  // struct Game Games[games];
-  // Game *games = loadGames(fgames,num);
-  // FILE *out = fopen("output.txt","w");
-  // fwrite(&games,sizeof(Game),3,out);
-  // fclose(out);
-  // struct Player Players[players];
-
-  
-  // int fplayrerLen = fseek(fplayer, 0L, SEEK_END);
-  // int sz = ftell(fplayer);
-  // printf("fplayer %d , f sz %d\n",fplayrerLen,sz);
-  // rewind(fplayer);
+  int fplayrerLen = fseek(fplayer, 0L, SEEK_END);
+  int sz = ftell(fplayer);
+  printf("fplayer %d , f sz %d\n",fplayrerLen,sz);
+  rewind(fplayer);
+  int games = numberOfGames(fgames);
   // printf("sizeof int %lu",sizeof(int));
   // printf("game matches: %lu",games);
   // int players=malloc(sizeof(fplayer)*4);
   // int players[sizeof(fplayer)];
   // printf("players length %d\n",sizeof(players));
-  // assert(id);
+  if(fplayer==NULL || fgames==NULL){
+    printf("couldn't open file");
+    return 1;
+  }
+  assert(id);
   // assert(fplayer);
   // assert(fgames);
   // load chars
   
   // 44 = ',' 59= ";"
 // allocating size for players
-  
+  for(char c=getc(fplayer);c!=EOF;c=getc(fplayer)){
+  if( c == '\n'){
+    nmOfLines++;
+  }}
 
-//   struct Player **Players = malloc(sizeof( player)*nmOfLines);
-//   rewind(fplayer);
+  *Players = malloc(sizeof( player)*nmOfLines);
+  rewind(fplayer);
   
-//   for(char c=getc(fplayer);c!=EOF;c=getc(fplayer)){
+  for(char c=getc(fplayer);c!=EOF;c=getc(fplayer)){
 
-//   // TODO not finished loading of data
-//   // missing inputing to array, just ints
-//   // https://stackoverflow.com/questions/10204471/convert-char-array-to-a-int-number-in-c
-//   if(c!=-2 || c != 10 || c!= 13 || c !=44){
-//       Players[user][2].name[charCount]=c;
-//       charCount++;
-//   }
-//   if (before==-2 || before==10){
-//     count=0;
-//     while(c!=44){
-//       printf("Num: %c\n",c);
-//       id[count] = c;
-//       c=getc(fplayer);
-//       // players[]=
-//       count++;
-//     }
-//   id[count]=0;
+  // TODO not finished loading of data
+  // missing inputing to array, just ints
+  // https://stackoverflow.com/questions/10204471/convert-char-array-to-a-int-number-in-c
+  if(c!=-2 || c != 10 || c!= 13 || c !=44){
+      Players[user].name[charCount]=c;
+      charCount++;
+  }
+  if (before==-2 || before==10){
+    count=0;
+    while(c!=44){
+      printf("Num: %c\n",c);
+      id[count] = c;
+      c=getc(fplayer);
+      // players[]=
+      count++;
+    }
+  id[count]=0;
     
-//     Players[user]->id = strtol(id,0,10);
+    Players[user].id = strtol(id,0,10);
 
-//     // Vynulovat id
-//     for(int i =0; i<=7;i++){
-//       id[i]=0;
-//     }
+    // Vynulovat id
+    for(int i =0; i<=7;i++){
+      id[i]=0;
+    }
 
-//     user++;
-//     charCount =0;
-//   }
-//     // printf("%c",c);
-//     before = c;
-//   }
+    user++;
+    charCount =0;
+  }
+    // printf("%c",c);
+    before = c;
+  }
 
 
-// // free(players);
-// return 0;
+// free(players);
+return 0;
 
 }
 
